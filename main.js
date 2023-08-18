056271be4cce790eb0c51d5590bbeb3f4d659d1b
LAWAL AL-AMEEN <141585256+ololade19@users.noreply.github.com>
2023-08-18 09:04:26 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+let num = 0;
+
+const value = document.querySelector(".value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            num--;
+        }else if(styles.contains("increase")) {
+            num++;
+        } else {
+            num = 0;
+        } 
+        value.textContent = num;
+
+
+
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
